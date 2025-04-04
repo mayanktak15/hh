27e10737af7ad0ff6d39ecaa70a82c9e0bd97def
mayanktak15 <mayanktakpc@gmail.com>
2025-04-05 04:45:58 +0530

    trending --- bugs

--- src/components/TrendingItem.jsx
+++ src/components/TrendingItem.jsx
@@ -1,25 +1,33 @@
 import "./TrendingSlider.css";
 import { items } from "./AllData";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function TrendingItem() {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
   const filteredItems = items.filter((item) => item.id >= 8);
+
+  const handleProductClick = (productId, e) => {
+    e.preventDefault();
+    navigate(`/categories/product/${productId}`);
+  };
+
   return (
     <>
       {filteredItems.map((item) => (
-        <div key={item.id} className="row-item">
-          <Link
-            onClick={() => window.top(0, 0)}
-            to={`/categories/product/${item.id}`}
-          >
-            <div className="item-header">
-              <img src={item.img} alt="product" />
-            </div>
-            <div className="item-description">
-              <p>{item.description}</p>
-              <p className="item-price">₹{item.price}</p>
-            </div>
-          </Link>
+        <div 
+          key={item.id} 
+          className="row-item"
+          onClick={(e) => handleProductClick(item.id, e)}
+        >
+          <div className="item-header">
+            <img src={item.img} alt="product" />
+          </div>
+          <div className="item-description">
+            <p>{item.description}</p>
+            <p className="item-price">₹{item.price}</p>
+          </div>
         </div>
       ))}
     </>
