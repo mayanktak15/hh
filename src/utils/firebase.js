7a3d1d1ae8ec9291637b279e246bbbd4b4f0d6bc
mayanktak15 <mayanktakpc@gmail.com>
2025-04-05 05:48:45 +0530

    database url

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -14,6 +14,7 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
